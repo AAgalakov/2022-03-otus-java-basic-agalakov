bc616c8d1bb458035b12e34af5671e0dd974ddbf
Andrey Agalakov <aagalakov@lincolnlabs.co>
2022-07-20 14:27:42 +0300

    06 homework

---
+++ src/main/java/hw06/MyAutoClose.java
@@ -0,0 +1,13 @@
+package hw06;
+
+public class MyAutoClose implements AutoCloseable {
+
+	public void throwException(){
+		throw new RuntimeException("Bad thing");
+	}
+
+	@Override
+	public void close() throws Exception {
+
+	}
+}
